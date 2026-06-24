54a51148735d2db689af887fba231bae6263a7b9
Math2710 <matheuswestphal47@gmail.com>
2026-06-24 11:24:03 -0300

    Java: Exercícios - Scanner

---
+++ Scanner/ex1_dados_pessoais.java
@@ -0,0 +1,28 @@
+package Scanner;
+import java.util.Scanner;
+public class ex1_dados_pessoais {
+    public static void main(String[] args) {
+        
+        Scanner n1 = new Scanner(System.in);
+        String nome; 
+        System.out.println("Digite seu Nome: ");
+        nome = n1.nextLine();
+
+        System.out.println("Digite sua idade"); 
+        int idade;
+        idade = n1.nextInt();
+
+        double altura;
+        System.out.println("Digite sua altura");
+        altura = n1.nextDouble();
+        
+
+
+        System.out.printf("Olá %s! você tem %d anos e %.2fm de altura.",nome, idade, altura);
+
+        n1.close();
+
+    }
+
+    
+}
